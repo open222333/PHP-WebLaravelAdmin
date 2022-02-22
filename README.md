7eb2368d8fbba44ab258ab4f07d5483a75395f89
tom_li <open222333@gmail.com>
2022-02-22 15:05:22 +0800

    自製 建立 laravel-admin sh腳本

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # PHP-laravel-admin
 laravel-admin 練習
+
+php版本需為7.4
+
+.env.example	laravel-admin設定
+sh setup_laravel_admin.sh
---
+++ setup_laravel_admin.sh
@@ -0,0 +1,31 @@
+# 安裝composer
+# php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"  
+# php composer-setup.php 
+# php -r "unlink('composer-setup.php');"
+# sudo mv composer.phar /usr/local/bin/composer
+
+# 取得當前資料夾
+path=`cd $(dirname $0);pwd -P`
+
+if [ -d "$path/code" ]
+then
+	# mysql 需先建立好資料庫
+	echo "安裝laravel-admin"
+	cd code
+	php artisan admin:install
+else
+	# 建立laravel專案 php 7.4
+	mkdir code
+	composer create-project --prefer-dist laravel/laravel  "code" 6.*
+
+	# 安裝laravel-admin前置準備
+	cd code
+	composer require encore/laravel-admin
+	php artisan vendor:publish --provider="Encore\Admin\AdminServiceProvider"
+
+	# 複製.env 並在執行一次
+	cd ..
+	cp .env.example code/.env
+	cp Dockerfile code/Dockerfile
+	sh setup_laravel_admin.sh
+fi
